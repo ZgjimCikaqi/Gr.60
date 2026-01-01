4abf9c7f905f8084875965a68bf274aa8971159e
VeseCikaqi <zgjim.cikaqi@student.uni-pr.edu>
2026-01-01 18:39:58 +0100

    feat: Implement stock increase logic with limits and input validation

--- Gr60/Gr60/Gr60.cpp
+++ Gr60/Gr60/Gr60.cpp
@@ -8,5 +8,33 @@ int main() {
     int samsung = 1;
     int huawei = 2;
 
+    char choice;
+
+    cout << "Zgjidhni nje nga brendet e listuara A - Apple, S - Samsung, H - Huawei? ";
+    cin >> choice;
+    choice = toupper(choice);
+
+    int* stock = nullptr;
+    string brand;
+
+    if (choice == 'A') {
+        stock = &apple;
+        brand = "Apple";
+    }
+    else if (choice == 'S') {
+        stock = &samsung;
+        brand = "Samsung";
+    }
+    else if (choice == 'H') {
+        stock = &huawei;
+        brand = "Huawei";
+    }
+    else {
+        cout << "Brend i panjohur\n";
+        return 0;
+    }
+
+    cout << "Keni zgjedhur telefon te brendit " << brand << ".\n";
+
     return 0;
 }
